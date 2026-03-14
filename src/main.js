7bfb66462676e85faa6a2acc9808792ee46e7822
Gyuwon-Jung <licmm0520@gmail.com>
2026-03-15 01:14:11 +0900

    [fix] 코드래빗 피드백 반영 (#132)

--- src/main.js
+++ src/main.js
@@ -7,6 +7,12 @@ import './assets/main.css'
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
-router.isReady().then(() => {
-  app.mount('#app')
-})
+router
+  .isReady()
+  .then(() => {
+    app.mount('#app')
+  })
+  .catch((error) => {
+    console.error('Router initialization failed. Mounting app anyway.', error)
+    app.mount('#app')
+  })
